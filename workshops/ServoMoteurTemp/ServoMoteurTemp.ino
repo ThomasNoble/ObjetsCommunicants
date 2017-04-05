813142e6efee22348bc2b1b8ac307ff9c5fc7b93
Sylvain Choquet <sylchoquet@gmail.com>
2017-04-05 11:52:36 +0200

    updated ServoMoteur

--- workshops/ServoMoteurTemp/ServoMoteurTemp.ino
+++ workshops/ServoMoteurTemp/ServoMoteurTemp.ino
@@ -1,17 +1,15 @@
 #include <Servo.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-  
-const int TEMP_SENSOR = 2;
 
-OneWire oneWire(TEMP_SENSOR);
-Servo myservo;
+#define ONE_WIRE_BUS 2 
+#define MOTEUR 5
 
+OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 DeviceAddress sensorDeviceAddress;
 
-int MOTEUR = 5;
-
+Servo myservo;
 double temp = 90;
 
 void setup(){    
@@ -19,27 +17,13 @@ void setup(){
   Serial.begin(9600);
   myservo.attach(MOTEUR);
   myservo.write(temp);
-
   sensors.begin();
-  sensors.getAddress(sensorDeviceAddress, 0); //Demande l'adresse du capteur à l'index 0 du bus
- sensors.setResolution(sensorDeviceAddress, 12);
 }
   
 void loop(){
-  
-    /*
-    temp = analogRead(TEMP_SENSOR)*(5000/1024);
-    Serial.println(temp);
-    temp = (temp - 500) * 10.0;
-    Serial.println(temp);
-    myservo.write(temp);
-    */
-    
-  //float reading = analogRead(TEMP_SENSOR); 
-  //Serial.println(convert(reading));
-
   sensors.requestTemperatures(); //Demande la température aux capteurs
- Serial.print("La température est: ");
- Serial.print(sensors.getTempCByIndex(0)); //Récupération de la température en celsius du capteur n°0
- Serial.println(" C°");
+  temp = sensors.getTempCByIndex(0);
+  Serial.print("La température est: ");
+  Serial.print(temp);
+  Serial.println(" C°");
 }
